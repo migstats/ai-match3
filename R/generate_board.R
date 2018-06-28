f1031bff9bde149a3f2cab043f5482a3a2710e2f
mporras <mporras@generagames.com>
2018-06-28 13:16:31 +0200

    nuevas funciones

--- R/generate_board.R
+++ R/generate_board.R
@@ -1,4 +1,5 @@
 library(purrr)
+library(magrittr)
 
 have_same_elements <- function(list){
   min(list) == max(list)
@@ -14,10 +15,23 @@ have_row_match <- function(list){
 }
 
 have_no_combination <- function(matrix){
-  every(matrix, ~ !have_row_match(.))
+  check_row <- function(matrix){
+    map(seq_len(ncol(matrix)), ~ matrix[,.]) %>% 
+      every(~ !have_row_match(.))
+  }
+  check_row(matrix) & check_row(t(matrix))
 }
 
 generate_matrix <- function (ncols = 5, nrows = 5, ncolours = 4){
-  
-  
+  sample(ncolours, ncols * nrows, replace = TRUE) %>% 
+    matrix(nrows, ncols)
+}
+
+generate_valid_matrix <- function(ncols = 5, nrows = 5, ncolours = 4){
+  gen_matrix <- matrix(0, nrows, ncols)
+  while(!have_no_combination(gen_matrix)){
+    gen_matrix <- generate_matrix(ncols, nrows, ncolours)
+    have_no_combination(gen_matrix)
+  }
+  gen_matrix
 }
